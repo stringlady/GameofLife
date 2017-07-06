b81ab8ac4c56fc26a521d21ac51297015bb333e0
Anaya Bussey <anayabussey@Anayas-MacBook-Air.local>
2017-07-06 14:22:39 -0400

    Initial Commit

---
+++ GameofLifeTests/GameofLifeTests.swift
@@ -0,0 +1,36 @@
+//
+//  GameofLifeTests.swift
+//  GameofLifeTests
+//
+//  Created by Anaya Bussey on 7/6/17.
+//  Copyright © 2017 Anaya Bussey. All rights reserved.
+//
+
+import XCTest
+@testable import GameofLife
+
+class GameofLifeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
